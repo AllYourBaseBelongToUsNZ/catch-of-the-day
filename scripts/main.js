b854dacd5a95aaf15ea10850b5a52eb45fd351db
AllYourBaseBelongToUsNZ <lauriewilliams@Lauries-MacBook-Pro.local>
2016-04-20 19:25:05 +1200

    Adding in some basic components
    Components for Store name, Order and Inventory. Not yet dynamic.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,88 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+
+var App = React.createClass({
+
+	render: function() {
+
+		return(
+
+		<div className="catch-of-the-day">
+			
+			<div className = "menu">
+				<Header tagline="A demo Seafood Market App"/>
+				</div>
+				<Order/>
+				<Inventory/>
+
+
+		</div>
+
+		)
+	}
+
+});
+
+//header component
+
+var Header = React.createClass({
+
+	render : function(){
+
+		console.log(this.props);
+
+		return(
+
+			<header className="top">
+
+				<h1>Catch 
+				<span className = "ofThe">
+				<span ClassName="of">of</span> 
+				<span ClassName="the">the</span> 
+				</span>
+				day</h1>
+				<h3 className="tagline">{this.props.tagline}</h3>
+
+
+			</header>
+
+		)
+	}
+});
+
+
+//inventory component
+
+var Inventory = React.createClass({
+
+	render : function(){
+
+		return(
+
+			<p>Inventory</p>
+
+		)
+	}
+});
+
+//Order component
+
+var Order = React.createClass({
+
+	render : function(){
+
+		return(
+
+			<p>Order</p>
+
+		)
+	}
+});
 
 /* Store picker component */
 
@@ -10,7 +93,11 @@ var StorePicker = React.createClass({
 //rendering HTML
 	render : function(){
 		return(
-			<p>hello</p> 
+			<form className="store-selector">
+				<h2>Please enter a store </h2>
+				<input type ="text" ref="storeID" required/>
+				<input type = "Submit"/> 
+			</form>
 		)
 	}
 
@@ -18,7 +105,8 @@ var StorePicker = React.createClass({
 
 //mount it to the page on page load and put in in the main div
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main') );
+ReactDOM.render(<App/>, document.querySelector('#main') );
+
 
 
  
